ccff30cc1fda5a9d7b72dff45b214f282bca4db9
GNamimates <greenneon188@gmail.com>
2023-05-31 01:33:30 +0800

    initial commit

---
+++ main.lua
@@ -0,0 +1,11 @@
+local parts = {
+   base=models.RCcar.Base,
+   steer_wheels = {
+      models.RCcar.Base.FL,
+      models.RCcar.Base.FR,
+   },
+   engine_wheels = {
+      models.RCcar.Base.BL,
+      models.RCcar.Base.BR,
+   }
+}
