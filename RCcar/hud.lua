d0d90cc3ad41c421d9beeda5ad87dd18c755f20f
GN <gnamimates188@gmail.com>
2023-06-05 23:26:27 +0800

    Speed Indicator

---
+++ RCcar/hud.lua
@@ -0,0 +1,11 @@
+if not host:isHost() then return end
+local RC = require("RCcar.RCmain")
+
+events.WORLD_RENDER:register(function (delta)
+   local d = RC:getEngineThrottleData()
+   local s = math.floor(d.z*30)
+   local kmph = math.floor(math.abs(RC:getLocalVel(delta).z) * 20 * 60 * 60 / 1000)
+   if RC:isActive() then
+      host:setActionbar('[{"color":"red","text":"' .. ('|'):rep(s) .. '"},{"text":"' .. ('|'):rep(30-s) .. '","color":"black"},{"text":" ' .. (kmph) .. ' km/h","color":"white"}]')
+   end
+end)
