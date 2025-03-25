87b01cafb469ae41b7a21bdc6409862cc65768d7
GNamimates <81744903+GNamimates@users.noreply.github.com>
2025-03-26 02:02:45 +0800

    new nameplate

---
+++ RCcar/nameplate.lua
@@ -0,0 +1,3 @@
+nameplate.ALL:setText(":racecar:${name}${badges}")
+nameplate.ENTITY:setBackgroundColor(0,0,0,0)
+nameplate.ENTITY:setOutline(true)
--- RCcar/racecarNameplate.lua
+++
@@ -1,4 +0,0 @@
-events.ENTITY_INIT:register(function ()
-   local name = player:getName()
-   nameplate.ALL:setText(":racecar:"..name)
-end)
