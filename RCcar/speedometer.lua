fc325601876b60cc6b086ea2ccb728e9b63b8afe
GNamimates <81744903+GNamimates@users.noreply.github.com>
2025-03-26 15:49:07 +0800

    speedometer

--- RCcar/speedometer.lua
+++ RCcar/speedometer.lua
@@ -4,7 +4,7 @@ local RC = require("RCcar.RCmain")
 events.WORLD_RENDER:register(function (delta)
    local d = RC:getEngineThrottleData()
    local s = math.floor(d.z*30)
-   local kmph = math.floor(math.abs(RC:getLocalVel(delta).z) * 20 * 60 * 60 / 1000)
+   local kmph = math.floor(math.abs(RC:getLocalVel(delta).z) * 72)
    if RC:isActive() then
       host:setActionbar('[{"color":"red","text":"' .. ('|'):rep(s) .. '"},{"text":"' .. ('|'):rep(30-s) .. '","color":"black"},{"text":" ' .. (kmph) .. ' km/h","color":"white"}]')
    end
